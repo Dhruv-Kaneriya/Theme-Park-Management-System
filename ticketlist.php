75c56ace06e2042154682cbd6130e1faca820198
Juzar10 <juzar786bharmal@gmail.com>
2022-03-06 19:52:34 +0400

    display ticket list done

--- ticketlist.php
+++ ticketlist.php
@@ -56,11 +56,7 @@
                                     </svg>
                                 </div>
                             </div>
-                            <div class="flex justify-between ml-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="#0066FF">
-                                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                </svg>
+                            <div class="ml-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="#FF0000">
                                     <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                                 </svg>
@@ -81,57 +77,35 @@
                                     </tr>
                                 </thead>
                                 <tbody class="text-gray-700">
-                                    <tr>
-                                        <td class=" text-left py-3 px-4 whitespace-nowrap ">
-                                            <input type="checkbox" name="name" class=" w-5 h-5 mr-2 bg-gray-50 rounded border border-gray-400 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
-                                            <span>Batman</span>
-                                        </td>
-                                        <td class="text-left py-3 px-4">1/2/2021 6:43PM</td>
-                                        <td class="text-left py-3 px-4">1</td>
-                                        <td class="text-left py-3 px-4">2</td>
+                                    <?php
+                                    // path is from relative to dashboard 
+                                    include "Database/db_conn.php";
 
-                                        <td class="text-left py-3 px-4">Roller Coaster</td>
-                                    </tr>
-                                    <tr class="bg-gray-100">
-                                        <td class=" text-left py-3 px-4">
-                                            <input type="checkbox" name="name" class=" w-5 h-5 mr-2 bg-gray-50 rounded border border-gray-400 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
-                                            <span>Batman</span>
-                                        </td>
-                                        <td class="text-left py-3 px-4">1/2/2021 6:43PM</td>
-                                        <td class="text-left py-3 px-4">1</td>
-                                        <td class="text-left py-3 px-4">2</td>
+                                    $sql = "SELECT tickets_list.ticket_id, ride_list.ride_name, tickets_list.customer_name,tickets_list.no_adult,tickets_list.no_child,tickets_list.date_issued from ride_list,tickets_list where ride_list.ride_id=tickets_list.ride_id; ";
 
-                                        <td class="text-left py-3 px-4">Roller Coaster</td>
-                                    </tr>
-                                    <tr>
-                                        <td class=" text-left py-3 px-4">
-                                            <input type="checkbox" name="name" class=" w-5 h-5 mr-2 bg-gray-50 rounded border border-gray-400 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
-                                            <span>Batman</span>
-                                        </td>
-                                        <td class="text-left py-3 px-4">1/2/2021 6:43PM</td>
-                                        <td class="text-left py-3 px-4">1</td>
-                                        <td class="text-left py-3 px-4">2</td>
 
-                                        <td class="text-left py-3 px-4">Roller Coaster</td>
-                                    </tr>
-                                    <tr class="bg-gray-100">
-                                        <td class=" text-left py-3 px-4">
-                                            <input type="checkbox" name="name" class=" w-5 h-5 mr-2 bg-gray-50 rounded border border-gray-400 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
-                                            <span>Batman</span>
-                                        </td>
-                                        <td class="text-left py-3 px-4">1/2/2021 6:43PM</td>
-                                        <td class="text-left py-3 px-4">1</td>
-                                        <td class="text-left py-3 px-4">2</td>
+                                    $res = mysqli_query($conn, $sql);
+                                    if ($res->num_rows > 0) {
+                                        while ($row = mysqli_fetch_assoc($res)) {
+                                    ?>
+                                            <tr>
+                                                <td class=" text-left py-3 px-4 whitespace-nowrap ">
+                                                    <input type="checkbox" name="name" class=" w-5 h-5 mr-2 bg-gray-50 rounded border border-gray-400 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
+                                                    <span><?php echo $row['customer_name'] ?></span>
+                                                </td>
+                                                <td class="text-left py-3 px-4"><?php echo $row['date_issued'] ?></td>
+                                                <td class="text-left py-3 px-4"><?php echo $row['no_adult'] ?></td>
+                                                <td class="text-left py-3 px-4"><?php echo $row['no_child'] ?></td>
 
-                                        <td class="text-left py-3 px-4">Roller Coaster</td>
-                                    </tr>
-                                    <tr class="bg-gray-200 text-sm">
-                                        <td colspan="5" class="text-right  py-3 px-4">
-                                            <span class=" mx-2 text-gray-400"> previous</span>
-                                            <span class="mx-2 text-gray-700">2</span>
-                                            <span class="mx-2 text-blue-600">next</span>
-                                        </td>
-                                    </tr>
+                                                <td class="text-left py-3 px-4"><?php echo $row['ride_name'] ?></td>
+                                            </tr>
+                                    <?php
+                                        }
+                                    }
+
+                                    ?>
+
+
                                 </tbody>
                             </table>
                         </div>
