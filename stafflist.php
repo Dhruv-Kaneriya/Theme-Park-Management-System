f7aa16db594791736465ec9aec4f06a0baebc2a7
Dhruv <dhruvkaneriya@rocketmail.com>
2022-04-12 16:26:07 +0530

    Delete Staff Completed

--- stafflist.php
+++ stafflist.php
@@ -44,7 +44,7 @@
                               $count_fetch = mysqli_fetch_array($count_result);
                               $postCount = $count_fetch['allcount'];
                               $limit = 3;
-                              $sql = "SELECT firstname, email, type FROM users LIMIT 0," . $limit;
+                              $sql = "SELECT id,firstname, email, type FROM users LIMIT 0," . $limit;
 
 
                               $res = mysqli_query($conn, $sql);
@@ -77,10 +77,11 @@
                                              <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                                              <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
                                           </svg>
-
+                                          <a href="util/deletestaff.php?id=<?=$row['id']?>" onclick='return deleterecord()'>
                                           <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="#FF0000">
                                              <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                                           </svg>
+                                          </a>
                                        </td>
                                     </tr>
 
@@ -104,7 +105,7 @@
             </div>
          </div>
       </div>
-   </div>
+   
    <script>
       $(document).ready(function() {
          $(document).on('click', '#loadBtn', function() {
@@ -142,6 +143,11 @@
       });
    </script>
 
+<script>
+   function deleterecord(){
+      return confirm('Are you sure you want to delete this record?');
+   }
+   </script>  
 </body>
 
 </html>
